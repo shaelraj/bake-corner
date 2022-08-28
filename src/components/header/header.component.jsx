0076e995d281d5d618b5e0678e4c57682082a549
Shaelraj <shael.dhn88@gmail.com>
2022-08-28 15:18:18 +0530

    implemented user redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./header.styles.scss";
 import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase.utils";
+import { connect } from "react-redux/es/exports";
 
 import { ReactComponent as Logo } from "../../asserts/crown.svg";
 
@@ -33,4 +34,8 @@ const Header = ({ currentUser }) => {
   );
 };
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
